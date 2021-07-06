c445154acd6e7136997c644b400400d56161bae6
Stephen <stephen@stephenbehnfeldt.com>
2021-07-06 06:15:58 -0400

    Generate Propel User and UserQuery classes.
    First classes for ORM, not used yet in body of program.  Generation of classes also generates two files, config.php and loadDatabase.php, that we incorporate into bootstrap.php.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -29,5 +29,40 @@ function bootstrap(string $configFile = '../config.json')
     }
     ini_set('display_errors', 'Off');
 
+    $host = $config['propel']['host'];
+    $dbname = $config['propel']['dbname'];
+    $user = $config['propel']['uname'];
+    $password = $config['propel']['pword'];
+
+    $serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+    $serviceContainer->checkVersion(2);
+    $serviceContainer->setAdapterClass('webapp', 'mysql');
+    $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
+    $manager->setConfiguration(array(
+        'classname' => 'Propel\\Runtime\\Connection\\ConnectionWrapper',
+        'dsn' => "mysql:host=$host;dbname=$dbname",
+        'user' => $user,
+        'password' => $password,
+        'attributes' =>
+            array(
+                'ATTR_EMULATE_PREPARES' => false,
+                'ATTR_TIMEOUT' => 30,
+            ),
+        'model_paths' =>
+            array(
+                0 => 'src',
+                1 => 'vendor',
+            ),
+    ));
+    $manager->setName('attend');
+    $serviceContainer->setConnectionManager('webapp', $manager);
+    $serviceContainer->setDefaultDatasource('webapp');
+
+    $serviceContainer->initDatabaseMaps(array (
+        'webapp' =>
+            array (
+                0 => '\\Sbehnfeldt\\Webapp\\PropelDbEngine\\Map\\UserTableMap',
+            ),
+    ));
     return $config;
 }
